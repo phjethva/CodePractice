07281773005513c950c908bde1efacfbcba60a47
Pratik Jethva <phjethva@gmail.com>
2021-09-25 13:22:10 +0530

    Programs updated

---
+++ java/A0001_HelloWorld.java
@@ -0,0 +1,10 @@
+package com.phjethva.codepractice.java;
+
+public class A0001_HelloWorld {
+
+    public static void main(String[] args) {
+        String msg = "Hello Java!";
+        System.out.print(msg);
+    }
+
+}
---
+++ kotlin/A0001_HelloWorld.kt
@@ -0,0 +1,6 @@
+package com.phjethva.codepractice.kotlin
+
+fun main(args: Array<String>) {
+    val msg = "Hello Kotlin!"
+    println(msg)
+}
